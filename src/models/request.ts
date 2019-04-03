67447e5a752c8db1c757e13af842b0cfc78ca21c
Satit Riapit <siteslave@msn.com>
2019-04-03 14:03:39 +0700

    update

--- src/models/request.ts
+++ src/models/request.ts
@@ -3,8 +3,25 @@ import * as Knex from 'knex';
 export class RequestModel {
 
   saveRequest(db: Knex, data: any) {
-    return db('requests')
+    return db('request')
       .insert(data);
+  }
+
+  getRequest(db: Knex) {
+    return db('request as r')
+      .select(
+        'r.request_date',
+        'r.request_time',
+        'r.status',
+        'r.symptom',
+        'p.cid',
+        'p.hn',
+        'p.first_name',
+        'p.last_name',
+        'p.sex',
+        'p.birthdate'
+      )
+      .innerJoin('patient as p', 'p.cid', 'r.cid')
   }
 
 }
